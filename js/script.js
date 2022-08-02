05aa9e615509f903d8633d150b9d3bf2a449e769
Debashis <debashisojha98@gmail.com>
2022-08-02 18:08:59 +0530

    new project added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation 
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Software Developer","Web Developer","Fresher"],
+    typeSpeed:100,
+    BackSpeed:50,
+    loop:true
+})
---
+++ test.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    s=x+y
+    return s 
+
+n1 = int(input("Enter a nuber to add: "))
+n2 = int(input("Enter another number to add: "))
+sum = add(n1,n2)
+print("The sum will be:",sum)
